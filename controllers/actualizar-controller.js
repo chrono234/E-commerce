c20e3ca929de4178c0d9157ade576117820f3474
Jairo Orozco <jairoor01@outlook.com>
2022-11-30 20:00:43 -0600

    update actualizar-controller file

--- controllers/actualizar-controller.js
+++ controllers/actualizar-controller.js
@@ -1,51 +1,47 @@
 import { productoServices } from "../services/productos-servicios.js";
 
-// const formUpdate = document.querySelector("[form-update]")
+const formUpdate = document.querySelector("[form-update]")
 
-// const obtenerInformacionProducto = async () => {
-//   const url = new URL(window.location);
-//   const id = url.searchParams.get("id");
+const obtenerInformacionProducto = async () => {
+const url = new URL(window.location);
+const id = url.searchParams.get("id");
 
-//   if (id == null) {
-//       // window.location.href = "/screens/error.html"
-//   }
+if (id == null) {
+      // window.location.href = "/screens/error.html"
+}
+const urlProducto = document.querySelector("[url-producto]");
+const categoria = document.querySelector("[categoria-producto]");
+const nombreProducto = document.querySelector("[nombre-producto]");
+const descripcion = document.querySelector("[descripcion-producto]");
 
-//   const urlProducto = document.querySelector("[url-producto]");
-//   const categoria = document.querySelector("[categoria-producto]");
-//   const nombreProducto = document.querySelector("[nombre-producto]");
-//   const descripcion = document.querySelector("[descripcion-producto]");
+try{
+    const perfil = await productoServices.actualizarProducto(id);
 
-//   try{
-//     const perfil = await productoServices.detalleCliente(id);
+    if (perfil.nombreProducto && perfil.email) {
+    nombre.value = perfil.nombre;
+    email.value = perfil.email;
+    }else{
+    throw new Error();
+    }
+}catch(error){
+    // window.location.href = "/screens/error.html";
+    console.log(error);
+}
 
-//     if (perfil.nombreProducto && perfil.email) {
-//       nombre.value = perfil.nombre;
-//       email.value = perfil.email;
-//     }else{
-//       throw new Error();
-//     }
-//   }catch(error){
-//     // window.location.href = "/screens/error.html";
-//     console.log(error);
-//   }
 
-  
-// }
+}
 
-// obtenerInformacionProducto()
+obtenerInformacionProducto()
 
-// formUpdate.addEventListener("submit", (evento) => {
-//   evento.preventDefault();
-//   const url = new URL(window.location);
-//   const id = url.searchParams.get("id");
-//   const urlProducto = document.querySelector("[url-producto]").value
-//   const categoria = document.querySelector("[categoria-producto]").value
-//   const nombreProducto = document.querySelector("[nombre-producto]").value
-//   const descripcion = document.querySelector("[descripcion-producto]").value
-//   productoServices.actualizarCliente(urlProducto,categoria,nombreProducto,descripcion).then(() => {
-//       window.location.href = "/screens/edicion_concluida.html"
-//   })
-// });
-
-const url = new URL (window.location);
-console.log(url.searchParams.get("id"));
+formUpdate.addEventListener("submit", (evento) => {
+evento.preventDefault();
+const url = new URL(window.location);
+const id = url.searchParams.get("id");
+const urlProducto = document.querySelector("[url-producto]").value
+const categoria = document.querySelector("[categoria-producto]").value
+const nombreProducto = document.querySelector("[nombre-producto]").value
+const descripcion = document.querySelector("[descripcion-producto]").value
+productoServices.actualizarCliente(urlProducto,categoria,nombreProducto,descripcion).then(() => {
+    window.location.href = "/screens/edicion_concluida.html"
+})
+});
